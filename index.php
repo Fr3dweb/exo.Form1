3ac543fe178044358f9653158ba374b1e69c981a
Fr3dplan <fr3d.web@gmail.com>
2022-09-06 11:40:50 +0200

    exo.Form1 exo5 terminé

--- index.php
+++ index.php
@@ -1,34 +1,65 @@
 <?php
 
-class Form {
+class Form
+{
+    protected string $form;
 
-    public string $form;
-
-    function  __construct(string $method) {
-        $this -> form = "<form method=$method action='votreréponseest'>";
+    public function __construct(string $method)
+    {
+        $this->form = "<form method=$method>";
     }
 
     public function setText(string $type, string $name, string $placeholder): void
     {
-        $this -> form .= "<input type=$type name=$name placeholder=$placeholder>";
+        $this->form .= "<input type=$type name=$name placeholder=$placeholder>";
     }
 
     public function setSubmit(): void
     {
-        $this -> form .= "<button type='submit'>Submit</button>";
+        $this->form .= "<button type='submit'>Submit</button>";
     }
 
-    public function getForm()
+    public function getForm(): string
     {
-        return $this -> form .= "</form>";
+        return $this->form .= "</form>";
     }
 }
 
 $form = new Form("get");
-$form->setText("text","Tomate","Valider");
-$form -> setSubmit();
-echo $form -> getForm();
+$form->setText("text", "Tomate", "Valider");
+$form->setSubmit();
+echo $form->getForm();
 
 var_dump($_POST);
 var_dump($_GET);
+
+
+class Form2 extends Form
+{
+    public function setRadio(string $name): void
+    {
+        $this -> form .= "<label>$name</label>";
+        $this-> form .= "<input type='radio' name='$name'>";
+    }
+
+
+    public function setCheckbox(): void
+    {
+        $this -> form .= "<input type='checkbox'>";
+    }
+}
+
+$form2 = new Form2("post");
+$form2 ->setRadio("Voulez valider ?");
+$form2 -> setRadio("Cocher svp");
+$form2 ->setCheckbox();
+$form2 -> setSubmit();
+echo $form2 -> getForm();
+
+var_dump($_POST);
+
+
+
+
+
 
